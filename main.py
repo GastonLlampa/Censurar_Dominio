245cdfdc19f7ec000a163fb669a8dc7de3e43c8a
Gaston Llampa <gastonsitto.ll@gmail.com>
2023-09-27 01:04:19 -0300

    cargando todito

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import re
-from Textos import *
 
-texto_prueba = "Este es un ejemplo de una página web: https://proyectodalto.com y también podemos visitar http://example.org"
+#def reWrite():
 
-preUrl = re["www""https\:\/\/wwww"]
+print("Ingrese url a eliminar")
+url_delete = input()
+patron1 = 'www\.'+url_delete+'\.[a-z]{2,3}'
+patron2 = 'https:\/\/www\.'+url_delete+'\.[a-z]{2,3}'
+patron3 = 'https:\/\/'+url_delete+'\.[a-z]{2,3}'
+patron4 = 'http:\/\/www\.'+url_delete+'\.[a-z]{2,3}'
+patron5 = 'http:\/\/'+url_delete+'\.[a-z]{2,3}'
+patron6 = url_delete+'\.[a-z]{2,3}'
+patron_completo = patron1+'|'+patron2+'|'+patron3+'|'+patron4+'|'+patron5+'|'+patron6
 
-postUrl = re["com"]
- 
-patron_completo = "{preUrl}?+\.[a-zA-Z0-9.-]+\.{postUrl}"
+texto = open('texto_1.txt', 'r')
+lineas = texto.readlines()
+for item in lineas:
+    #resultado = re.findall(patron_completo, item)
+    resultado = re.search(patron_completo, item)
+    star_url = resultado.start()
+    fin_url = resultado.end()
+    i = 0
+    lista = list(item)
+    for caracter in lista:
+        if(star_url <= i & i < fin_url):
+            lista[i] = '-'
+        i = i + 1
+    item = ''.join(lista)
+    print(item)
+    #texto.write(item)
+    print(resultado)
+    #reWrite()
 
-resultado = re.findall(patron_completo, texto_prueba)
-
-print(resultado)
+texto.close()
