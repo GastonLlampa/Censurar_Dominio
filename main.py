ae1da377c1c4b2f8985e78fca5dbf8c88cd69a08
FernandoB <benitezf.3105@gmail.com>
2023-10-07 20:26:18 -0300

    Implementacoin tkinter, ajustes varios

--- main.py
+++ main.py
@@ -1,31 +1,70 @@
 import re
+import tkinter as tk
+from tkinter import filedialog
 
-#def reWrite():
+def censurarDominio():
+    #Proceso de eliminacion del dominio
+    url_delete = dominio_eliminar.get()
+    patron1 = 'www\.'+url_delete+'\.[a-z]{2,3}'
+    patron2 = 'https:\/\/www\.'+url_delete+'\.[a-z]{2,3}'
+    patron3 = 'https:\/\/'+url_delete+'\.[a-z]{2,3}'
+    patron4 = 'http:\/\/www\.'+url_delete+'\.[a-z]{2,3}'
+    patron5 = 'http:\/\/'+url_delete+'\.[a-z]{2,3}'
+    patron6 = url_delete+'\.[a-z]{2,3}'
+    patron_completo = patron1+'|'+patron2+'|'+patron3+'|'+patron4+'|'+patron5+'|'+patron6
 
-print("Ingrese url a eliminar")
-url_delete = input()
-patron1 = 'www\.'+url_delete+'\.[a-z]{2,3}'
-patron2 = 'https:\/\/www\.'+url_delete+'\.[a-z]{2,3}'
-patron3 = 'https:\/\/'+url_delete+'\.[a-z]{2,3}'
-patron4 = 'http:\/\/www\.'+url_delete+'\.[a-z]{2,3}'
-patron5 = 'http:\/\/'+url_delete+'\.[a-z]{2,3}'
-patron6 = url_delete+'\.[a-z]{2,3}'
-patron_completo = patron1+'|'+patron2+'|'+patron3+'|'+patron4+'|'+patron5+'|'+patron6
+    lineas = archivo.read()
+    if archivo:
+        lineas=re.sub(patron_completo, '-', lineas, flags=re.IGNORECASE)
+        ventana_texto = lineas 
+        texto_resul.config(text=lineas)
+        boton_exportar.config(state="active")   #Activa el boton de exportar archivo
 
-texto = open('texto_1.txt', 'r')
-lineas = texto.read()
 
-#resultado = re.search(patron_completo, item, flags=re.IGNORECASE)
-print("Archivo Inicial")
-print(lineas)
-lineas=re.sub(patron_completo, '-', lineas, flags=re.IGNORECASE)
-print("Archivo resultante")
-print(lineas)
-print("Desea reescribir el archivo? Si | NO")
-op = input()
-if (op.lower() == 'si'):
-    archivo = open('texto_1.txt', 'w')
-    archivo.write(lineas)
-    archivo.close()
-    print("Archivo Reescrito")
-texto.close()
+def exportar():
+    #Si desea exportar el archivo de texto, se debe seleccionar un nuevo archivo
+    nuevo_archivo = filedialog.asksaveasfilename(defaultextension=".txt")
+    with open(nuevo_archivo, "w") as file:
+        file.write(texto_resul.get())
+
+def seleccionar_archivo():
+    #Seleccionar archivo de texto
+    global archivo
+    archivo = filedialog.askopenfile()
+    if archivo:
+        mensaje_archivo_selecc.config(text=f"Archivo seleccionado: {archivo.name}")
+    else:
+        mensaje_archivo_selecc.config(text="Archivo no seleccionado")
+
+
+menu = tk.Tk()
+menu.title("Eliminar Dominio")
+menu.geometry("600x300")
+
+mensaje_dominio = tk.Label(menu, text="Ingrese el dominio a eliminar")
+mensaje_dominio.pack()
+
+dominio_eliminar = tk.Entry(menu)
+dominio_eliminar.pack()
+mensaje_archivo = tk.Label(menu, text="Ingrese el archivo a revisar")
+mensaje_archivo.pack()
+
+boton_archivo = tk.Button(menu, text="Seleccionar..", command=seleccionar_archivo)
+boton_archivo.pack()
+
+mensaje_archivo_selecc = tk.Label(menu, text="")
+mensaje_archivo_selecc.pack()
+
+boton_revisar = tk.Button(menu, text="Eliminar Dominio", command=censurarDominio)
+boton_revisar.pack()
+
+texto_resul = tk.Label(menu, text="")
+texto_resul.pack()
+
+boton_exportar = tk.Button(menu, text="Reescribir archivo", command=exportar, state=tk.DISABLED)
+boton_exportar.pack()
+
+mensaje_exportar = tk.Label(menu, text="")
+mensaje_exportar.pack()
+
+menu.mainloop()
